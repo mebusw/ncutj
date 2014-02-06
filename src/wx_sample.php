a2d970f81e0ccde41e09c87736f833a9ce1c38e0
nashe <contact@nashe.fr>
2014-02-06 14:13:39 +0100

    Avoid XSS
    Hi,
    I added some calls to htmlspecialchars(), since some $_GET data is echo'ed.

--- src/wx_sample.php
+++ src/wx_sample.php
@@ -5,116 +5,96 @@
 
 
 // ------ Settings & Includes ----------
-header("content-type:text/html; charset=utf-8");
+header('content-type:text/html; charset=utf-8');
 // require_once("config.php");
 
 //define your token
-define("TOKEN", "weixin");
+define('TOKEN', 'weixin');
 $wechatObj = new wechatCallbackapiTest();
 $wechatObj->valid();
 
 class wechatCallbackapiTest
 {
-	public function valid()
+    public function valid()
     {
-        if( isset($_REQUEST['echostr']) ) {
-            $echoStr = $_GET["echostr"];
+        if (isset($_REQUEST['echostr'])) {
+            $echoStr = htmlspecialchars($_GET['echostr']);
             echo $echoStr;
-            exit;
+        } 
+        else { 
+            echo $this->checkSignature();
+            $this->responseMsg();
         }
-
-        echo $this->checkSignature();
-
-        $this->responseMsg();
         exit;
-
     }
 
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+		$postStr = $GLOBALS['HTTP_RAW_POST_DATA'];
 
       	//extract post data
-		if (!empty($postStr)){
-                
-          	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $msgType = trim($postObj->MsgType);
-            $event = trim($postObj->Event);
-            $time = time();
+		if (!empty($postStr)) {
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = htmlspecialchars($postObj->FromUserName));
+                $toUsername =  htmlspecialchars($postObj->ToUserName);
+                $keyword =  htmlspecialchars(trim($postObj->Content));
+                $msgType =  htmlspecialchars(trim($postObj->MsgType));
+                $event =  htmlspecialchars(trim($postObj->Event));
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+                            <Event><![CDATA[%s]]></Event>
+							</xml>";    
 
-            switch ($msgType) {
-                case "event":
-                    switch ($event) {
-                        case 'subscribe':
-                            $contentStr = "欢迎关注中央大学天津校友会。";
-                            break;
-                        case 'unsubscribe':
-                            $contentStr = "谢谢光临。";
-                            break;
-                    }                        
-                    break;
+                switch ($msgType) {
+                    case 'event':
+                     # code...
+                     break;
+                    case 'text':
+                        break;
+                    default:
+                     # code...
+                     break;
+                 }                                     
 
-                case "text":
-                    switch ($keyword) {
-                        case '1':
-                            $contentStr = "最近尚未安排活动，敬请期待。";
-                            break;
-                        case '2':
-                            $contentStr = "请留下你微信号。";
-                            break;
-                        default:
-                            $contentStr = "回复数字:\n1.获取最新活动安排。\n2.申请加入微信大群。\n";
-                            break;
-                    }
-                    break;
 
-                default:
-                    break;
-            } 
+            	$contentStr = 'Welcome to wechat world!';
+            	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr, $event);
+            	echo $resultStr;
 
-            $textTpl = "<xml>
-                <ToUserName><![CDATA[%s]]></ToUserName>
-                <FromUserName><![CDATA[%s]]></FromUserName>
-                <CreateTime>%s</CreateTime>
-                <MsgType><![CDATA[%s]]></MsgType>
-                <Content><![CDATA[%s]]></Content>
-                <FuncFlag>0</FuncFlag>
-                <Event><![CDATA[%s]]></Event>
-                </xml>";
-            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr, $event);
-            echo $resultStr;
-
-        } else {
-        	echo "No POST raw data.";
+        } 
+        else {
+        	echo 'No POST raw data.';
         	exit;
         }
     }
 		
 	private function checkSignature()
 	{
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];	
+        $signature = htmlspecialchars($_GET['signature']);
+        $timestamp = htmlspecialchars($_GET['timestamp']);
+        $nonce = htmlspecialchars($_GET['nonce']);	
         		
 		$token = TOKEN;
 
-        echo "=".$signature.$timestamp.$nonce.$token."=";
+        echo '=' . $signature . $timestamp . $nonce . $token . '=';
 
 		$tmpArr = array($token, $timestamp, $nonce);
 		sort($tmpArr);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
+		$tmpStr = implode($tmpArr);
+		$tmpStr = sha1($tmpStr);
 		
-		if( $tmpStr == $signature ){
+		if ($tmpStr == $signature) {
 			return true;
-		}else{
+		} 
+		else {
 			return false;
 		}
 	}
 }
-
-?>
